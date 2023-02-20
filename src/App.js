e746191db77ac9478e00d47a39e1b322c65e4848
Gianluca La Manna <gianluca.lamanna@it.clara.net>
2023-02-20 18:11:16 +0100

    remove tag p

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ function App() {
       <header className="App-header">
         {PROJECT_NAME && <div>{PROJECT_NAME}</div>}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
